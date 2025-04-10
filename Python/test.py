a2c4c83a488e597e1ed1fe60fae1b497a580a24b
Genaro Ricci <genaroricci2@gmail.com>
2025-04-09 22:05:09 -0300

    Poco de powershell, practica con lista

---
+++ Python/test.py
@@ -0,0 +1,32 @@
+import time
+import math
+import random
+errormsg = 'Error: Debes introducir un numero valido.'
+nm0msg = 'Introduce un numero mayor a 0'
+
+###################################################################
+
+
+
+##############################################
+
+##############################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+time.sleep(3)
+

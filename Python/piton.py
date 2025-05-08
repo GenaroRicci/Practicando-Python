7a22e5af2d5da1d46b1406003ed96fd896804f4d
Genaro Ricci <genaroricci2@gmail.com>
2025-05-08 00:59:02 -0300

    Actualizando nomas

--- Python/piton.py
+++ Python/piton.py
@@ -531,16 +531,17 @@ print(inp[:20])
 ###########################################################
 #Startswith para busqueda smart
 try:
-    manejador_archivo = open('mbodx.txt')
-    counter = 0
+    manejador_archivo = open('mbox.txt')
+    counter_lineas = 0
     for linea in manejador_archivo:
         linea = linea.rstrip()#Elimina todos los espacios en blanco a la derecha de una cadenas
         if linea.startswith('From:'):
+            counter_lineas = counter_lineas + 1
             print(linea)
+    print(f'El total de lineas de redactores es: {counter_lineas}')
 except FileNotFoundError:
     print('No se pudo acceder al archivo')
-############################################################
-#
+#########################################################
 
 
 
--- Python/test.py
+++ Python/test.py
@@ -5,11 +5,17 @@ errormsg = 'Error: Debes introducir un numero valido.'
 nm0msg = 'Introduce un numero mayor a 0'
 
 ###################################################
-manejador_archivo = open('mbox.txt')
-inp = manejador_archivo.read()
-print(len(inp))
-print(inp[:20])
-
+try:
+    manejador_archivo = open('mbox.txt')
+    counter_lineas = 0
+    for linea in manejador_archivo:
+        linea = linea.rstrip()#Elimina todos los espacios en blanco a la derecha de una cadenas
+        if linea.startswith('From:'):
+            counter_lineas = counter_lineas + 1
+            print(linea)
+    print(f'El total de lineas de redactores es: {counter_lineas}')
+except FileNotFoundError:
+    print('No se pudo acceder al archivo')
 #####################################################
 
 time.sleep(3)
